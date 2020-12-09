49cc8c75141f6b140c14ea351030ab0a6b97aa35
benjamin <benjamin.leyrat.24@gmail.com>
2020-12-09 20:14:25 +0100

    Conf DB test

--- src/DataFixtures/CardFixtures.php
+++ src/DataFixtures/CardFixtures.php
@@ -8,13 +8,18 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class CardFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        for($i = 1; $i <= 3; $i++){
+    const LENGTH = 3;
+
+    public function load(ObjectManager $manager){
+        $descriptions = ["Ceci est une petite description", 
+                            "Cela est une seconde description",
+                            "Cela est une dernière description"];
+
+        for($i = 1; $i <= self::LENGTH; $i++){
             $card = new Card();
-            $card->setTitle('title_'.$i)
-                ->setImg('img_'.$i.'jpeg')
-                ->setDescription('desc_'.$i);
+            $card->setTitle('title '.$i)
+                ->setImg('img_'.$i.'.jpeg')
+                ->setDescription($descriptions[$i - 1]);
             $manager->persist($card);
         }
         $manager->flush();
---
+++ tests/CardTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Tests;
+
+use App\DataFixtures\CardFixtures;
+use App\Repository\CardRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CardTest extends KernelTestCase{
+
+    use FixturesTrait;
+
+    public function testCount(){
+        self::bootKernel();
+        $this->loadFixtures([CardFixtures::class]);
+        $cards = self::$container->get(CardRepository::class)->count([]);
+        $this->assertEquals(3, $cards);
+    }
+}
